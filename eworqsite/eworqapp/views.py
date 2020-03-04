38126dcc20f0a35b7c58b49c2ecf75e42a470f38
diarmuidclarke <32582190+diarmuidclarke@users.noreply.github.com>
2020-03-04 22:49:10 +0000

    part3 partial - templates and views

--- eworqsite/eworqapp/views.py
+++ eworqsite/eworqapp/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import eWORQ_Request
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the eworqapp index.")
+    
+    active_eworq_list = eWORQ_Request.objects.order_by('-eWORQ_raised_date')[:5]
+    template = loader.get_template('eworqapp/index.html')
+    context = {
+        'active_eworq_list': active_eworq_list,
+    }
+    return HttpResponse(template.render(context, request))
